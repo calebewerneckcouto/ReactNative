e7f15d7f53292f6510cdca29ad69158f227f2d17
CALEBE WERNECK COUTO <59000781+calebewerneckcouto@users.noreply.github.com>
2024-07-27 19:42:44 -0300

    Update card.jsx

--- src/pages/card.jsx
+++ src/pages/card.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './card.css'
 
 function Card({title, image}) {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [currentCommand, setCurrentCommand] = useState('');
+	const openModal = () => {
+        setCurrentCommand();
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
 	return(
-			<div className='card'>
-                <h4>{title}</h4>
-				<img className='cardImage' src={image}/>
+			<div className='card' >
+                <h4>{title} </h4>
+				<img className='cardImage' src={image} onClick={openModal}/>
+				{isModalOpen && (
+                <div className="modal">
+                    <div className="modal-content" onClick={closeModal}>
+                        <span className="close-button" >&times;</span>
+						<h2>{currentCommand}</h2>
+						<h4>{title}</h4>
+						<img className='cardImage' src={image}/>
+                        
+                        
+                    </div>
+                </div>
+            )}
 			</div>
+			
     )
 }
 
